fa568bf1af8c146120282e3555124002d274b25b
James Johnson <jtaylorjohnson@gmail.com>
2020-04-05 07:20:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,7 @@ class Dog
   
   def initialize(name)
     @name = name
-    @@all << self
+    self.save
   end
   
   def self.all 
@@ -17,6 +17,10 @@ class Dog
     @@all.each do |name|
     puts (name)
     end
+  end
+  
+  def save
+    @@all << self 
   end
   
 end
